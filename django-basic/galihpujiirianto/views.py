97cee50e2c99c7f918c1843faa11563ee55cf16c
galihpujiirianto <galihpujiirianto@gmail.com>
2022-06-29 16:51:35 +0700

    mulai dari 0 ya kak

--- django-basic/galihpujiirianto/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def useRender(request):
-    return render(request, 'index.html')
-
-def useHttpResponse(request):
-    return HttpResponse("Response ini dibuat dengan HttpResponse")
--- navbar/navbarjs/script.js
+++
@@ -1,3 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle input')
-const nav = document.querySelector('nav ul')
-menuToggle.addEventListener('click', () => nav.classList.toggle('slide'))
