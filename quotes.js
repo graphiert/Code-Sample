df3e731c111da33561ac81cc82e307a14b624cc8
galihpujiirianto <galihpujiirianto@gmail.com>
2022-07-20 15:36:31 +0700

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -1,8 +1,26 @@
-const axios = require("axios")
+// With NodeJS
+const { request } = require("http");
 const getQuote = () => {
-  const apiEndpoint = "https://api.quotable.io/random";
-  const params = { minLength: 5, maxLength: 65 };
-  axios.get(apiEndpoint, { params })
-    .then(res => res.data.content)
-    .then(res => console.log(res)); // Do anything
+  request({
+    host: "api.quotable.io",
+    path: "/random?maxLength=65"
+  }, res => {
+    let value;
+    res.on('data', stuff => {
+      let conc = Buffer.concat([stuff]);
+      let fromStr = conc.toString("utf-8");
+      value = JSON.parse(fromStr).content;
+    });
+    res.on('end', () => console.log(value)); // Do anything
+  }).end();
+};
+
+// With Web API
+const getQuote = () => {
+  fetch("https://api.quotable.io/random?maxLength=65")
+  .then(res => res.json())
+  .then(res => {
+    let data = data.content;
+    console.log(data); // Do anything
+  });
 };
